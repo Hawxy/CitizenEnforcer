1440de8f05a188eaf9fb8f0dc525d5949a27e980
JT <Hawxy@users.noreply.github.com>
2018-10-01 21:30:14 +0800

    Remove unused owner config item (#5)

--- CitizenEnforcer/Settings/Configuration.cs
+++ CitizenEnforcer/Settings/Configuration.cs
@@ -23,7 +23,6 @@ namespace CitizenEnforcer.Settings
 {
     public class Configuration
     {
-        public ulong[] Owner { get; set; }
         public string Prefix { get; set; }
         public string Token { get; set; } 
         [JsonIgnore]
